994ff4e6e237aa42c079f31fd6cb0f000e203667
Elisa Ajxup Yax <lisajxupyax@gmail.com>
2024-03-20 12:39:24 -0600

    Login_Interfaz_Completo

--- Corpoagrima/src/main/java/corpoagrima/corpoagrima/Corpoagrima.java
+++ Corpoagrima/src/main/java/corpoagrima/corpoagrima/Corpoagrima.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package corpoagrima.corpoagrima;
 
-/**
- *
- * @author lisaj
- */
 public class Corpoagrima {
 
     public static void main(String[] args) {
