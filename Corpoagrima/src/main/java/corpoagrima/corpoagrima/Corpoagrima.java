e7ba4081f9c3807070c4ab2eaefcc31c26158a89
Elisa Ajxup Yax <lisajxupyax@gmail.com>
2024-03-20 10:07:02 -0600

    Correcion de error

---
+++ Corpoagrima/src/main/java/corpoagrima/corpoagrima/Corpoagrima.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package corpoagrima.corpoagrima;
+
+/**
+ *
+ * @author lisaj
+ */
+public class Corpoagrima {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
