4d6208108b36d01864466f0af581e16418ee2411
WilderL <menchuwilder@gmail.com>
2024-03-30 17:12:17 -0600

    implementacion de metodos para el manejo de la tabla puesto

--- Corpoagrima/src/main/java/corpoagrima/corpoagrima/bdMariaDB/ConexionPuesto.java
+++ Corpoagrima/src/main/java/corpoagrima/corpoagrima/bdMariaDB/ConexionPuesto.java
@@ -2,21 +2,71 @@
 package corpoagrima.corpoagrima.bdMariaDB;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author WilderL
  */
 public class ConexionPuesto {
-    public ResultSet consulta(Connection conexion){
+    public ResultSet consulta(Connection conexion) throws SQLException{
         String sql = "SELECT * FROM Puesto";
-        return null;
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        return stmt.executeQuery();
     }
     
-    public ResultSet puestos(Connection conexion, String nombre){
-        String sql = String.format("SELECT Nombre FROM Puesto WHERE nombre=%s", nombre);
-        return null;
+    public ResultSet puestos(Connection conexion) throws SQLException{
+        String sql = "SELECT Nombre FROM Puesto";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        return stmt.executeQuery();
+    }
+    
+    public ResultSet puesto(Connection conexion, String nombre) throws SQLException{
+        String sql = "SELECT Nombre FROM Puesto WHERE Nombre LIKE ?";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setString(1, "%" + nombre + "%");
+        return stmt.executeQuery();
+    }
+    
+    public boolean agregar(Connection conexion,String nombre, String horario, 
+            String descripcion, float salarioBase) throws SQLException{
+        String sql = "INSERT INTO Puesto "
+                + "(Nombre, Horario, Descripcion, Salario_Base) "
+                + "VALUES (?, ?, ?, ?)";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setString(1, nombre);
+        stmt.setString(2, horario);
+        stmt.setString(3, descripcion);
+        stmt.setFloat(4, salarioBase);
+        // ejecutar la consulta
+        int filasAfectadas = stmt.executeUpdate();
+        return filasAfectadas > 0;
+    }
+    
+    public boolean actualizar(Connection conexion,String nombre, String horario, 
+            String descripcion, float salarioBase, int id)throws SQLException{
+        String sql = "UPDATE Puesto SET Nombre=?, Horario=?, Descripcion=?,"
+                + " Salario_Base=? WHERE ID_Puesto=?";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setString(1, nombre);
+        stmt.setString(2, horario);
+        stmt.setString(3, descripcion);
+        stmt.setFloat(4, salarioBase);
+        stmt.setInt(5, id);
+        // ejecutar la consulta
+        int filasAfectadas = stmt.executeUpdate();
+        return filasAfectadas > 0;
+    }
+    
+    public boolean eliminar(Connection conexion, int id) throws SQLException{
+        String sql = "DELETE FROM Puesto WHERE ID_Puesto=?";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setInt(1, id);
+        // ejecutar la consulta
+        int filasAfectadas = stmt.executeUpdate();
+        return filasAfectadas > 0;
     }
     
 }
