9aa02721e3ccb9982ff7962a236aab19b42405ec
WilderL <menchuwilder@gmail.com>
2024-03-31 01:28:13 -0600

    implementacin de metodos de consulta a la tabla cliente y correccion de consulta de guardar a la tabla puesto

--- Corpoagrima/src/main/java/corpoagrima/corpoagrima/bdMariaDB/ConexionCliente.java
+++ Corpoagrima/src/main/java/corpoagrima/corpoagrima/bdMariaDB/ConexionCliente.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 public class ConexionCliente {
     public ResultSet busqueda(Connection conexion, String textoBusqueda) throws SQLException{
 
-        String sql = "SELECT * FROM cliente WHERE Nombre LIKE ? OR ID_Cliente = ?";
+        String sql = "SELECT * FROM Cliente WHERE Nombre LIKE ? OR ID_Cliente = ?";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setString(1, "%" + textoBusqueda + "%");
         stmt.setString(2, textoBusqueda);
@@ -26,7 +26,7 @@ public class ConexionCliente {
     }
     public ResultSet busqueda2(Connection conexion, String textoBusqueda) throws SQLException{
 
-        String sql = "SELECT * FROM cliente WHERE ID_Cliente = ? OR NIT = ?";
+        String sql = "SELECT * FROM Cliente WHERE ID_Cliente = ? OR NIT = ?";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setString(1, textoBusqueda);
         stmt.setString(2, textoBusqueda);
@@ -35,31 +35,84 @@ public class ConexionCliente {
 
     }
     public ResultSet consulta(Connection conexion) throws SQLException{
-        String sql = "SELECT * FROM cliente";
+        String sql = "SELECT * FROM Cliente";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         
         return stmt.executeQuery();
     }
     
     public ResultSet ordenarNombre(Connection conexion) throws SQLException{
-        String sql = "SELECT * FROM cliente ORDER BY nombre ASC, apellido ASC";
+        String sql = "SELECT * FROM Cliente ORDER BY Nombre ASC, Apellido ASC";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         
         return stmt.executeQuery();
     }
     
     public ResultSet ordenarApellido(Connection conexion) throws SQLException{
-        String sql = "SELECT * FROM cliente ORDER BY apellido ASC, nombre ASC";
+        String sql = "SELECT * FROM Cliente ORDER BY Apellido ASC, Nombre ASC";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         
         return stmt.executeQuery();
     }
     
     public ResultSet ordenarDestacado(Connection conexion) throws SQLException{
-        String sql = "SELECT * FROM cliente ORDER BY Cliente_destacado DES";
+        String sql = "SELECT * FROM Cliente ORDER BY Cliente_destacado DES";
         PreparedStatement stmt = conexion.prepareStatement(sql);
-        
         return stmt.executeQuery();
+    }
+    
+    public boolean agregar(Connection conexion, int idCliente, String nombre, 
+            String apellido, String nit, String correoElectronico, 
+            String direccion, int clienteDestacado, int cantidadCompra) 
+            throws SQLException{
+        String sql = "INSERT INTO Cliente "
+                + "(ID_Cliente, Nombre, Apellido, NIT, Correo_Electronico,"
+                + "Direccion, Cliente_destacado, Cantidad_compras) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setInt(1, idCliente);
+        stmt.setString(2, nombre);
+        stmt.setString(3, apellido);
+        stmt.setString(4, nit);
+        stmt.setString(5, correoElectronico);
+        stmt.setString(6, direccion);
+        stmt.setInt(7, clienteDestacado);
+        stmt.setInt(8, cantidadCompra);
+        // ejecutar la consulta
+        int filasAfectadas = stmt.executeUpdate();
+        return filasAfectadas > 0;
+    }
+    
+    public boolean actualizar(Connection conexion, int idCliente, String nombre, 
+            String apellido, String nit, String correoElectronico, 
+            String direccion, int clienteDestacado, int cantidadCompra)
+            throws SQLException{
+        String sql = "UPDATE Empleado SET ID_Empleado=?, Nombre=?, Apellido=?, "
+                + "NIT=?, Correo_Electronico=?, Direccion=?, Ajuste_Sueldo=?, "
+                + "Bonificaciones=?, Puesto_ID_Puesto=?, Usuario_ID_Usuario=? "
+                + "WHERE ID_Empleado=?";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setInt(1, idCliente);
+        stmt.setString(2, nombre);
+        stmt.setString(3, apellido);
+        stmt.setString(4, nit);
+        stmt.setString(5, correoElectronico);
+        stmt.setString(6, direccion);
+        stmt.setInt(7, clienteDestacado);
+        stmt.setInt(8, cantidadCompra);
+        stmt.setInt(9, idCliente);
+        // ejecutar la consulta
+        int filasAfectadas = stmt.executeUpdate();
+        return filasAfectadas > 0;
+    }
+    
+    public boolean eliminar(Connection conexion, int id) throws SQLException{
+        String sql = "DELETE FROM Cliente WHERE ID_Cliente=?";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setInt(1, id);
+        // ejecutar la consulta
+        int filasAfectadas = stmt.executeUpdate();
+        return filasAfectadas > 0;
     }
 }
 
