f4d134f368a657e2a8381d22bae57922a3c2c78c
WilderL <menchuwilder@gmail.com>
2024-05-06 00:29:40 -0600

    se implemente consultas para compra y producto

--- Corpoagrima/src/main/java/corpoagrima/corpoagrima/bdMariaDB/ConexionCompra.java
+++ Corpoagrima/src/main/java/corpoagrima/corpoagrima/bdMariaDB/ConexionCompra.java
@@ -11,45 +11,56 @@ import java.sql.SQLException;
  */
 public class ConexionCompra {
     //public ResultSet 
-    
+
     public ResultSet consulta(Connection conexion) throws SQLException {
         String sql = "SELECT NoFactura, Proveedor.Empresa,  Fecha, Tipo_Compra, Total FROM Registro_Compra "
-            + "JOIN Proveedor ON Registro_Compra.Proveedor_ID_Proveedor = Proveedor.ID_Proveedor "
-            + "WHERE Anulado=?";
-        
+                + "JOIN Proveedor ON Registro_Compra.Proveedor_ID_Proveedor = Proveedor.ID_Proveedor "
+                + "WHERE Anulado=?";
+
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setBoolean(1, false);
-        
+
         return stmt.executeQuery();
     }
+
     public ResultSet busqueda(Connection conexion, String factura) throws SQLException {
         String sql = "SELECT rc.ID_Compra, rc.NoFactura, rc.Fecha, rc.Tipo_Compra, rc.Total, p.Empresa, p.NIT, e.Nombre, e.Apellido, rcp.Detalle "
-            + "FROM Registro_Compra rc "
-            + "JOIN Proveedor p ON rc.Proveedor_ID_Proveedor = p.ID_Proveedor "
-            + "JOIN Empleado e ON rc.Empleado_ID_Empleado = e.ID_Empleado "
-            + "LEFT JOIN Registro_Compra_has_Producto rcp ON rcp.Registro_Compra_ID_Compra = rc.ID_Compra "
-            + "WHERE rc.NoFactura=? LIMIT 1 ";
-        
+                + "FROM Registro_Compra rc "
+                + "JOIN Proveedor p ON rc.Proveedor_ID_Proveedor = p.ID_Proveedor "
+                + "JOIN Empleado e ON rc.Empleado_ID_Empleado = e.ID_Empleado "
+                + "LEFT JOIN Registro_Compra_has_Producto rcp ON rcp.Registro_Compra_ID_Compra = rc.ID_Compra "
+                + "WHERE rc.NoFactura=? LIMIT 1 ";
+
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setString(1, factura);
-        
+
         return stmt.executeQuery();
     }
+
     public ResultSet busqueda2(Connection conexion, String factura) throws SQLException {
         String sql = "SELECT Producto.Nombre, Producto.Descripcion, Producto.Marca, rcp.Cantidad_compra, rcp.Costo_Unidad, rcp.Costo_Total "
-            + "FROM Registro_Compra rc "
-            + "INNER JOIN Registro_Compra_has_Producto rcp ON rcp.Registro_Compra_ID_Compra = rc.ID_Compra "
-            + "INNER JOIN Producto ON rcp.Producto_ID_Producto = Producto.ID_Producto "  
-            + "WHERE rc.NoFactura=?";
-        
+                + "FROM Registro_Compra rc "
+                + "INNER JOIN Registro_Compra_has_Producto rcp ON rcp.Registro_Compra_ID_Compra = rc.ID_Compra "
+                + "INNER JOIN Producto ON rcp.Producto_ID_Producto = Producto.ID_Producto "
+                + "WHERE rc.NoFactura=?";
+
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setString(1, factura);
-        
+
         return stmt.executeQuery();
     }
-    public boolean agregar(Connection conexion, String noFactura, 
-            boolean anulado, String fecha, String tipoCompra, 
-            float total, int proveedor_id_proveedor, int empleado_id_empleado) throws SQLException{
+
+    public ResultSet idCompra(Connection conexion, String noFactura) throws SQLException {
+        String sql = "SELECT ID_Compra FROM Registro_Compra WHERE NoFactura=?";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setString(1, noFactura);
+
+        return stmt.executeQuery();
+    }
+
+    public boolean agregar(Connection conexion, String noFactura,
+            boolean anulado, String fecha, String tipoCompra,
+            float total, int proveedor_id_proveedor, int empleado_id_empleado) throws SQLException {
         String sql = "INSERT INTO Registro_Compra "
                 + "(NoFactura, Anulado, Fecha, Tipo_Compra,"
                 + "Total, Proveedor_ID_Proveedor, Empleado_ID_Empleado)"
@@ -66,10 +77,10 @@ public class ConexionCompra {
         int filasAfectadas = stmt.executeUpdate();
         return filasAfectadas > 0;
     }
-    
-    public boolean actualizar(Connection conexion, int id_Compra, String noFactura, 
-            boolean anulado, String fecha, String tipoCompra, 
-            float total, int proveedor_id_proveedor, int empleado_id_empleado)throws SQLException{
+
+    public boolean actualizar(Connection conexion, int id_Compra, String noFactura,
+            boolean anulado, String fecha, String tipoCompra,
+            float total, int proveedor_id_proveedor, int empleado_id_empleado) throws SQLException {
         String sql = "UPDATE Registro_Compra SET NoFactura=?, Anulado=?, Fecha=?, Tipo_Compra=?,"
                 + "Total=?, Proveedor_ID_Proveedor=?, Empleado_ID_Empleado=?"
                 + "WHERE ID_Compra=?";
@@ -86,8 +97,8 @@ public class ConexionCompra {
         int filasAfectadas = stmt.executeUpdate();
         return filasAfectadas > 0;
     }
-    
-    public boolean eliminar(Connection conexion, int id) throws SQLException{
+
+    public boolean eliminar(Connection conexion, int id) throws SQLException {
         String sql = "UPDATE Registro_Compra SET Anulado=? WHERE ID_Compra=?";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setBoolean(1, true);
@@ -96,13 +107,13 @@ public class ConexionCompra {
         int filasAfectadas = stmt.executeUpdate();
         return filasAfectadas > 0;
     }
-    
-    public boolean anular(Connection conexion, int id) throws SQLException{
+
+    public boolean anular(Connection conexion, int id) throws SQLException {
         String sql = "UPDATE Registro_Compra SET Anulado=? WHERE ID_Compra=?";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setBoolean(1, true);
         stmt.setInt(2, id);
-        
+
         int filasAfectadas = stmt.executeUpdate();
         return filasAfectadas > 0;
     }
--- Corpoagrima/src/main/java/corpoagrima/corpoagrima/bdMariaDB/ConexionProducto.java
+++ Corpoagrima/src/main/java/corpoagrima/corpoagrima/bdMariaDB/ConexionProducto.java
@@ -1,4 +1,3 @@
-
 package corpoagrima.corpoagrima.bdMariaDB;
 
 import java.sql.Connection;
@@ -12,7 +11,8 @@ import java.sql.SQLException;
  * @author WilderL
  */
 public class ConexionProducto {
-    public ResultSet busqueda(Connection conexion, String textoBusqueda) throws SQLException{
+
+    public ResultSet busqueda(Connection conexion, String textoBusqueda) throws SQLException {
 
         String sql = "SELECT * FROM Producto "
                 + "WHERE (Nombre LIKE ? OR ID_Producto = ?) AND Eliminado=?";
@@ -24,7 +24,8 @@ public class ConexionProducto {
         return stmt.executeQuery();
 
     }
-    public ResultSet busqueda2(Connection conexion, int id) throws SQLException{
+
+    public ResultSet busqueda2(Connection conexion, int id) throws SQLException {
 
         String sql = "SELECT * FROM Producto WHERE ID_Producto = ? AND Eliminado=?";
         PreparedStatement stmt = conexion.prepareStatement(sql);
@@ -34,7 +35,7 @@ public class ConexionProducto {
         return stmt.executeQuery();
 
     }
-    
+
     public ResultSet busqueda3(Connection conexion, String textoBusqueda) throws SQLException {
         String sql = "SELECT Producto.ID_Producto, Producto.Nombre, Producto.Descripcion, Producto.Marca, Producto.Categoria, "
                 + "Producto.Unidad_Medida FROM Producto "
@@ -46,19 +47,27 @@ public class ConexionProducto {
 
         return stmt.executeQuery();
     }
-    
-    public ResultSet consulta(Connection conexion) throws SQLException{
+
+    public ResultSet consulta(Connection conexion) throws SQLException {
         // Crear una sentencia SQL
         String sql = "SELECT * FROM Producto WHERE Eliminado=?";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setBoolean(1, false);
-        
+
         return stmt.executeQuery();
     }
     
-    public boolean agregar(Connection conexion, String nombre, String descripcion, String marca, 
-            String categoria, int cantidad, String unidad_medida, 
-            float precio_venta) throws SQLException{
+    public ResultSet cantidad(Connection conexion, String nombre) throws SQLException {
+        String sql = "SELECT ID_Producto, Stock FROM Producto WHERE Nombre=?";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setString(1, nombre);
+        
+        return stmt.executeQuery();
+    }
+
+    public boolean agregar(Connection conexion, String nombre, String descripcion, String marca,
+            String categoria, int cantidad, String unidad_medida,
+            float precio_venta) throws SQLException {
         String sql = "INSERT INTO Producto "
                 + "(Nombre, Descripcion, Marca, Categoria, Stock, Unidad_Medida, Precio_Venta, Eliminado)"
                 + "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
@@ -71,12 +80,12 @@ public class ConexionProducto {
         stmt.setString(6, unidad_medida);
         stmt.setFloat(7, precio_venta);
         stmt.setBoolean(8, false);
-        
+
         int filasAfectadas = stmt.executeUpdate();
         return filasAfectadas > 0;
     }
-    
-    public boolean eliminar(Connection conexion, int id) throws SQLException{
+
+    public boolean eliminar(Connection conexion, int id) throws SQLException {
         String sql = "UPDATE Producto SET Eliminado=? WHERE ID_Producto=?";
         PreparedStatement stmt = conexion.prepareStatement(sql);
         stmt.setBoolean(1, true);
@@ -85,25 +94,36 @@ public class ConexionProducto {
         int filasAfectadas = stmt.executeUpdate();
         return filasAfectadas > 0;
     }
-    
-    public boolean actualizar(Connection conexion, String nombre, String descripcion, String marca, 
-            String categoria, int cantidad, String unidad_medida, 
+
+    public boolean actualizar(Connection conexion, String nombre, String descripcion, String marca,
+            String categoria, int cantidad, String unidad_medida,
             float precio_venta, int id) throws SQLException {
-            String sql = "UPDATE Producto SET Nombre=?, Descripcion=?, Marca=?, "
-                    + "Categoria=?, Stock=?, Unidad_Medida=?, Precio_Venta=?, Eliminado=? WHERE ID_Producto=?";
-            
-            PreparedStatement stmt = conexion.prepareStatement(sql);
-            stmt.setString(1, nombre);
-            stmt.setString(2, descripcion);
-            stmt.setString(3, marca);
-            stmt.setString(4, categoria);
-            stmt.setInt(5, cantidad);
-            stmt.setString(6, unidad_medida);
-            stmt.setFloat(7, precio_venta);
-            stmt.setBoolean(8, false);
-            stmt.setInt(9, id);
-            
+        String sql = "UPDATE Producto SET Nombre=?, Descripcion=?, Marca=?, "
+                + "Categoria=?, Stock=?, Unidad_Medida=?, Precio_Venta=?, Eliminado=? WHERE ID_Producto=?";
+
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setString(1, nombre);
+        stmt.setString(2, descripcion);
+        stmt.setString(3, marca);
+        stmt.setString(4, categoria);
+        stmt.setInt(5, cantidad);
+        stmt.setString(6, unidad_medida);
+        stmt.setFloat(7, precio_venta);
+        stmt.setBoolean(8, false);
+        stmt.setInt(9, id);
+
         // ejecutar la consulta
+        int filasAfectadas = stmt.executeUpdate();
+        return filasAfectadas > 0;
+    }
+
+    public boolean actualizar(Connection conexion, int idProducto, int cantidad) 
+            throws SQLException {
+        String sql = "UPDATE Producto SET Stock=? WHERE ID_Producto=?";
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+        stmt.setInt(1, cantidad);
+        stmt.setInt(2, idProducto);
+        
         int filasAfectadas = stmt.executeUpdate();
         return filasAfectadas > 0;
     }
